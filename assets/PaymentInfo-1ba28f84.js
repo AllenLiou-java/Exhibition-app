5eee1595639074b0b177624a15fef5c3645a63b6
yuling <gb2013429@gmail.com>
2023-08-22 17:03:46 +0800

    Updates

---
+++ assets/PaymentInfo-1ba28f84.js
@@ -0,0 +1,1 @@
+import{P as t}from"./PaymentNavList-b85cbb7d.js";import{r as a,o as s,f as n,a as e,i as r}from"./index-e6ab8505.js";const c={class:"container"},_=r("p",null,"填寫付款資料",-1),d={__name:"PaymentInfo",setup(i){return(m,p)=>{const o=a("router-view");return s(),n("div",c,[e(t),e(o),_])}}};export{d as default};
